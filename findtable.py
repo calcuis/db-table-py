f9a7216785e27e3655587c148d2bc7f098bfdd7f
calcuis <113646141+calcuis@users.noreply.github.com>
2023-09-12 09:11:20 +0800

    Create findtable.py

---
+++ findtable.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+connection = sqlite3.connect("gta.db")
+cursor = connection.cursor()
+table_list = [a for a in cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table'")]
+print(table_list)
+
+connection.close()
